a87156785fb88c8ed9813bf166d3ccdf20628286
CraftModzZ Crafttino21 <58913870+Crafttino21@users.noreply.github.com>
2022-01-25 21:20:44 +0100

    Add Crackfield launcher sorce and Injection
    I have add the Crackfield 4 sorce and the reapzZ injector witch dosnt work yet

---
+++ injector.py
@@ -0,0 +1,17 @@
+import pyfiglet
+import os
+import time
+
+
+intro = pyfiglet.figlet_format("Reaper By CraftModzZ")
+print(intro)
+print("DOT CLOSE THIS WINDOW!, Stay safe that battelfield is in Borderless window mode!")
+
+
+time.sleep(8)
+os.startfile("C:\Program Files (x86)\Origin\HydraOverlay.exe")
+
+
+
+
+
---
+++ run.bat
@@ -0,0 +1,402 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpoS7czp5vyCnmkYR+krd5/n+LLaYOUL+knrfIIR1GNKmdgfQg1abXI=
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpoS7czp5vyCnmkYR+krd5/n+LLaYOUL+knrfIIR1GNKmdgfOkkWbgvL
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpoS7czp5vyCnmkYR+krd5/n+LLaYOUL+knrfIIRxH9Kl9kYLhRAMASpfW8=
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpoS7czp5vyCnmkYR+krd5/n+LLaYOUL+knrfIIR9HwO2OICGB5KcFKoYgYxp3pRikycM9WdlQDzSU3p
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpoS7czp5vyCnmkYR+krd5/n+LLaYOUL+knrfIIR9HwO2OICGB5KcFKoYgYxp3pRinTVONejsQm1dV7YqB5+Hn1x5w==
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpoS7czp5vyCnmkYR+krd5/n+LLaYOUL+knrfIIR9HwO2OICGB5KcFKoYgYxp3pRilaMNMySqQPeZ12F8kIYGmZ/vGHJsx8zZfxyg/445gODw3bqka4e3m/xXaELFHfyj6FtBfZO+BKxCA==
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZkoaHUrTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlXMbSXrZg==
+::ZQ05rAF9IAHYFVzEqQIzOhpXRwKLOWe+Zg==
+::eg0/rx1wNQPfEVWB+kM9LVsJDAOENG/a
+::fBEirQZwNQPfEVWB+kM9LVsJDAKFOGO+ALIUiA==
+::cRolqwZ3JBvQF1fEqQIZPAgUbxaDP2C8D7AQ7Ig=
+::dhA7uBVwLU+EWFqW9UQkBRRQVj7i
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBVdQg6DAES0A5EO4f7+086CsUYJW/IDW53a2b+IKeUf7gWwO5M10xo=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+color a
+title Crackfield 4 by CraftModzZ
+echo WWWWWWWW                           WWWWWWWW                             AAA                                                    DDDDDDDDDDDDD                                                                  
+echo W::::::W                           W::::::W                            A:::A                                                   D::::::::::::DDD                                                               
+echo W::::::W                           W::::::W                           A:::::A                                                  D:::::::::::::::DD                                                             
+echo W::::::W                           W::::::W                          A:::::::A                                                 DDD:::::DDDDD:::::D                                                            
+echo  W:::::W           WWWWW           W:::::W eeeeeeeeeeee             A:::::::::A          rrrrr   rrrrrrrrr       eeeeeeeeeeee    D:::::D    D:::::D     eeeeeeeeeeee  vvvvvvv           vvvvvvv  ssssssssss   
+echo   W:::::W         W:::::W         W:::::Wee::::::::::::ee          A:::::A:::::A         r::::rrr:::::::::r    ee::::::::::::ee  D:::::D     D:::::D  ee::::::::::::ee v:::::v         v:::::v ss::::::::::s  
+echo    W:::::W       W:::::::W       W:::::We::::::eeeee:::::ee       A:::::A A:::::A        r:::::::::::::::::r  e::::::eeeee:::::eeD:::::D     D:::::D e::::::eeeee:::::eev:::::v       v:::::vss:::::::::::::s 
+echo     W:::::W     W:::::::::W     W:::::We::::::e     e:::::e      A:::::A   A:::::A       rr::::::rrrrr::::::re::::::e     e:::::eD:::::D     D:::::De::::::e     e:::::e v:::::v     v:::::v s::::::ssss:::::s
+echo      W:::::W   W:::::W:::::W   W:::::W e:::::::eeeee::::::e     A:::::A     A:::::A       r:::::r     r:::::re:::::::eeeee::::::eD:::::D     D:::::De:::::::eeeee::::::e  v:::::v   v:::::v   s:::::s  ssssss 
+echo       W:::::W W:::::W W:::::W W:::::W  e:::::::::::::::::e     A:::::AAAAAAAAA:::::A      r:::::r     rrrrrrre:::::::::::::::::e D:::::D     D:::::De:::::::::::::::::e    v:::::v v:::::v      s::::::s      
+echo        W:::::W:::::W   W:::::W:::::W   e::::::eeeeeeeeeee     A:::::::::::::::::::::A     r:::::r            e::::::eeeeeeeeeee  D:::::D     D:::::De::::::eeeeeeeeeee      v:::::v:::::v          s::::::s   
+echo         W:::::::::W     W:::::::::W    e:::::::e             A:::::AAAAAAAAAAAAA:::::A    r:::::r            e:::::::e           D:::::D    D:::::D e:::::::e                v:::::::::v     ssssss   s:::::s 
+echo          W:::::::W       W:::::::W     e::::::::e           A:::::A             A:::::A   r:::::r            e::::::::e        DDD:::::DDDDD:::::D  e::::::::e                v:::::::v      s:::::ssss::::::s
+echo           W:::::W         W:::::W       e::::::::eeeeeeee  A:::::A               A:::::A  r:::::r             e::::::::eeeeeeeeD:::::::::::::::DD    e::::::::eeeeeeee         v:::::v       s::::::::::::::s 
+echo            W:::W           W:::W         ee:::::::::::::e A:::::A                 A:::::A r:::::r              ee:::::::::::::eD::::::::::::DDD       ee:::::::::::::e          v:::v         s:::::::::::ss  
+echo             WWW             WWW            eeeeeeeeeeeeeeAAAAAAA                   AAAAAAArrrrrrr                eeeeeeeeeeeeeeDDDDDDDDDDDDD            eeeeeeeeeeeeee           vvv           sssssssssss    
+                                                                                                                                                                                                              
+                                                                                                                                                                                                              
+                                                                                                                                                                                                              
+                                                                                                                                                                                                              
+                                                                                                                                                                                                              
+                                                                                                                                                                                                              
+                                                                                                                                                                                                              
+
+                                                         
+                                                         
+
+                                                                           
+
+echo Login for use Client!:
+set /p NA=enter USERNAME::
+set /p MM=enter PASSWORD::
+echo Version 1.7
+cls
+echo Hello %NA%. Thx for Use WeAreDev Injector By CraftModzZ! 
+echo Please wait a Littel bit!
+color c
+echo PLEASE READ README.TXT BEFOR USE!
+timeout /T 10 /nobreak
+color a
+cls
+:ans
+cls
+title Loader by CraftModzZ [Logged in as: %NA% ]
+echo.
+echo Choose you Mod %NA%!
+echo ===========
+echo.
+echo [1]p0op client (Free) (undetectet!)
+echo [2]Ricknzl MultiHack (Free) (need to be updatet! Dont use on custom servers!!)
+echo [3]HydraForce (Crack) (Undetectet) (High Custom Hack)
+echo [4]MyReport Script (Beta)
+echo [a]donate (PayPal)
+echo [b]Discord
+echo [c]Fairfight Bypass (For Extern Clients!)
+echo [d]Report Blocker (Beta)
+echo [p]Performenc Boost (FPS)
+echo [o]Official Site
+echo [e]Exit Loader
+echo [x]Deinject Clients
+echo.
+
+set asw=0
+set /p asw="Choose what you answare: "
+
+if %asw%==1 goto poop
+if %asw%==2 goto rick
+if %asw%==3 goto hydra
+if %asw%==a goto donate
+if %asw%==b goto discord
+if %asw%==c goto bypass
+if %asw%==x goto dekill 
+if %asw%==e goto end 
+if %asw%==d goto report
+if %asw%==4 goto myreport
+if %asw%==p goto fps
+if %asw%==o goto site
+goto ans
+
+
+:site
+start https://sites.google.com/view/crackfield-4/startseite
+goto ans
+
+:hydra
+title HydraForce Cracked By CraftModzZ
+cls
+echo check Detect Status!
+timeout /T 10 /nobreak
+color 9
+cls
+echo Status: Cracked! Undetectet!
+timeout /T 5 /nobreak
+color a
+echo Choose your injetion modet!
+echo ===========
+echo.
+echo [1]old Bypass
+echo [2]BetterBypass (Python requiret)
+
+echo.
+
+set asw=0
+set /p asw="choose what you want:"
+
+if %asw%==1 goto dev
+if %asw%==2 goto rep
+
+
+:dev
+
+taskkill /IM bf4.exe /F
+taskkill /IM Origin.exe /F
+copy HydraForce "C:\Program Files (x86)\Origin"
+
+cls
+echo ***********************************
+echo *                                 *
+echo *         Load Bypass             *
+echo *                                 *
+echo ***********************************
+start bypass.vbs
+timeout /T 15 /nobreak
+cd C:\Program Files (x86)\Origin
+start Origin.exe
+cls
+echo Start Battelfield Yet!
+echo .
+
+echo THE GAME NEED TO BE IN BORDERLESS MODE!!
+pause
+cls
+echo ***********************************
+echo *                                 *
+echo *   Download Client and Inject    *
+echo *                                 *
+echo ***********************************
+cd HydraForce
+start HydraOverlay.exe
+cls
+goto ans
+
+:rep
+taskkill /IM bf4.exe /F
+taskkill /IM Origin.exe /F
+copy HydraForce "C:\Program Files (x86)\Origin"
+
+cls
+python bypassV2.py 
+cls
+echo Start Battelfield Yet!
+echo .
+
+echo THE GAME NEED TO BE IN BORDERLESS MODE!!
+pause
+cls
+echo ***********************************
+echo *                                 *
+echo *   Download Client and Inject    *
+echo *                                 *
+echo ***********************************
+cd HydraForce
+start HydraOverlay.exe
+cls
+goto ans
+
+
+
+
+
+
+
+
+
+
+:fps
+cd C:\Windows\Temp
+del *.*
+goto ans
+
+:rick
+title Inject Ricknzl MultiHack by WeAreDevs injector by CraftModzZ
+cls
+echo check Detect Status!
+timeout /T 10 /nobreak
+cls
+echo Status: Undetectet!
+timeout /T 5 /nobreak
+taskkill /IM bf4.exe /F
+taskkill /IM Origin.exe /F
+echo ***********************************
+echo *                                 *
+echo *         Load Bypass             *
+echo *                                 *
+echo ***********************************
+start bypass.vbs
+timeout /T 15 /nobreak
+cls
+copy  Ricknzl_MultiHack[by_TheOzy][UC]_[unknowncheats.me]_.exe "C:\Program Files (x86)\Origin"
+echo ***********************************
+echo *                                 *
+echo *       Launch Battelfield        *
+echo *                                 *
+echo ***********************************
+cd C:\Program Files (x86)\Origin
+start Origin.exe
+echo press enter If Battelfield Startet! and you in 
+echo the main Screen!
+echo .
+echo THE GAME NEED TO BE IN BORDERLESS MODE!!
+pause
+cls
+echo ***********************************
+echo *                                 *
+echo *   Download Client and Inject    *
+echo *                                 *
+echo ***********************************
+start Ricknzl_MultiHack[by_TheOzy][UC]_[unknowncheats.me]_.exe
+cls
+goto ans
+
+
+
+
+
+
+
+:myreport
+echo Search Reports! Please Wait!
+timeout /T 5 /nobreak
+color c
+cls
+echo Report for Cheating by Clientsite AC
+echo (ID: %random%-%random%-%random%)
+pause
+cls
+color a
+echo delete Report and Start AntiRep
+echo Delet Report please Wait!
+timeout /T 20 /nobreak
+start reportBox.vbs
+cls
+goto ans
+
+:bypass 
+echo Launch Bypass (Only for Extern Clients)
+taskkill /IM Origin.exe
+taskkill /IM bf5.exe
+start bypass.vbs
+cls
+goto ans
+
+:discord
+start https://discord.gg/aQnStdMEjg
+cls
+goto ans
+
+:error
+color a
+echo This client is not to use this time!
+color a
+cls 
+goto ans
+
+:donate
+start https://paypal.me/crafttino21
+goto ans
+
+
+
+
+:poop
+title Inject poop by WeAreDevs injector by CraftModzZ
+cls
+echo check Detect Status!
+timeout /T 10 /nobreak
+cls
+
+echo Status: Undetectet!
+timeout /T 5 /nobreak
+taskkill /IM bf4.exe /F
+taskkill /IM Origin.exe /F
+echo ***********************************
+echo *                                 *
+echo *         Load Bypass             *
+echo *                                 *
+echo ***********************************
+start bypass.vbs
+timeout /T 15 /nobreak
+cls
+copy p0op_bf4_v1.5.exe "C:\Program Files (x86)\Origin"
+echo ***********************************
+echo *                                 *
+echo *       Launch Battelfield        *
+echo *                                 *
+echo ***********************************
+cd C:\Program Files (x86)\Origin
+start Origin.exe
+cls
+echo press enter If Battelfield Startet! and you in 
+echo the main Screen!
+pause
+cls
+echo ***********************************
+echo *                                 *
+echo *   Download Client and Inject    *
+echo *                                 *
+echo ***********************************
+start p0op_bf4_v1.5.exe
+cls
+goto ans
+
+:dekill
+taskkill /IM p0op_bf4_v1.5.exe /F
+taskkill /IM p0op_bf4_v1.5.exe /F
+taskkill /IM HydraOverlay.exe /F
+taskkill /IM Ricknzl_MultiHack[by_TheOzy][UC]_[unknowncheats.me]_.exe /F
+
+goto ans
+
+:report
+
+title Load ⱤɆ₱ØⱤ₮ ₭łⱠⱠɆⱤ
+echo ITS A BETA PROGRAMM!
+start reportBox.vbs
+cls
+goto ans
+
+
+
+
+:end
+taskkill /IM p0op_bf4_v1.5.exe /F
+taskkill /IM HydraOverlay.exe /F
+taskkill /IM Ricknzl_MultiHack[by_TheOzy][UC]_[unknowncheats.me]_.exe /F
+
+del bypass.vbs
+del Lizenz.txt 
+del p0op_bf4_v1.5.exe
+del HydraForce
+del Ricknzl_MultiHack[by_TheOzy][UC]_[unknowncheats.me]_.exe
+del reportBox.vbs
+cd C:\Program Files (x86)\Origin
+del p0op_bf4_v1.5.exe
+del HydraForce
+del Ricknzl_MultiHack[by_TheOzy][UC]_[unknowncheats.me]_.exe
+cls
+echo All cheats get Removed! Restart Launcher
+echo for inject New! 
+
+echo ****************************
+echo *                          *
+echo *                          *
+echo *       Thx for use        *
+echo *                          *
+echo *                          *
+echo ****************************
+timeout /T 12 /nobreak
+
